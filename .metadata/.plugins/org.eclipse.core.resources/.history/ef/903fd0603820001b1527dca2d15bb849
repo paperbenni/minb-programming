312dfd051d5d7a1a72b574735a92ca64f5d837be
paperbenni <paperbenni@gmail.com>
2020-11-12 18:27:52 +0100

    init

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/ef/903fd0603820001b1527dca2d15bb849
@@ -0,0 +1,28 @@
+	
+	// Datei Fahrenheit4.java
+	package k02_simpleprg;
+	
+	public class Fahrenheit4 {
+		/**
+		 * Geg.: Temperaturbereich in °F <br/>
+		 * Ges.: Temperaturliste in °C
+		 * 
+		 * @param args
+		 *            unused
+		 */
+		public static void main(String[] args) {
+	
+			final int UPPER = 250; // obere Grenze
+			final int LOWER =   0; // untere Grenze
+			final int STEP  =  25; // Schrittweite
+	
+			for (int fahrenheit = LOWER; fahrenheit <= UPPER; fahrenheit = fahrenheit + STEP )
+				//    Initialisierung  ; Bedingung          ; Fortsetzung: Schleifenvariable erhoehen			
+			{
+				int celsius = 5 * (fahrenheit - 32) / 9;
+				System.out.println(fahrenheit + "°F = " + celsius + "°C");
+			}
+		}
+	}
+
+	
---
+++ inf1/src/blatt02/Fahrenheit4.java
@@ -0,0 +1,36 @@
+	
+	// Datei Fahrenheit4.java
+	package blatt02;
+	
+	public class Fahrenheit4 {
+		/**
+		 * Geg.: Temperaturbereich in °F <br/>
+		 * Ges.: Temperaturliste in °C
+		 * 
+		 * @param args
+		 *            unused
+		 */
+		public static void main(String[] args) {
+	
+			final int UPPER = 250; // obere Grenze
+			final int LOWER =   0; // untere Grenze
+			final int STEP  =  25; // Schrittweite
+	
+            final int NUM_PER_ROW = 5;
+            int counter = 0;
+			
+			for (int fahrenheit = LOWER; fahrenheit <= UPPER; fahrenheit = fahrenheit + STEP )
+				//    Initialisierung  ; Bedingung          ; Fortsetzung: Schleifenvariable erhoehen			
+			{
+				int celsius = 5 * (fahrenheit - 32) / 9;
+                System.out.printf("%5d°F = %-5s", fahrenheit, celsius + "°C");
+                counter++;
+                if (counter >= NUM_PER_ROW) {
+                	counter = 0;
+                    System.out.printf("%n");
+                }
+			}
+		}
+	}
+
+	
