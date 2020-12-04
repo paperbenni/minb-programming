289d8d2b9b1670a6f414307d3178863eaae503f9
paperbenni <paperbenni@gmail.com>
2020-12-04 15:19:56 +0100

    change arg handling

--- inf1/src/blatt06/MySudoku.java
+++ inf1/src/blatt06/MySudoku.java
@@ -13,26 +13,29 @@ public class MySudoku {
   private final int BLOCK_SIZE = 3;
   private final int ROW_SIZE = BLOCK_SIZE * BLOCK_SIZE;
 
-  private int[][] spielFeld =
-      /* |---------|---------|---------| */
-      {{5, 3, 0, 0, 7, 0, 0, 0, 0},
-       {6, 0, 0, 1, 9, 5, 0, 0, 0},
-       {0, 9, 8, 0, 0, 0, 0, 6, 0},
-       /* |---------|---------|---------| */
-       {8, 0, 0, 0, 6, 0, 0, 0, 3},
-       {4, 0, 0, 8, 0, 3, 0, 0, 1},
-       {7, 0, 0, 0, 2, 0, 0, 0, 6},
-       /* |---------|---------|---------| */
-       {0, 6, 0, 0, 0, 0, 2, 8, 0},
-       {0, 0, 0, 4, 1, 9, 0, 0, 5},
-       {0, 0, 0, 0, 8, 0, 0, 7, 9}};
-
   public static void main(String[] args) {
+    int[][] testSpielFeld =
+        /* |---------|---------|---------| */
+        {{5, 3, 0, 0, 7, 0, 0, 0, 0},
+         {6, 0, 0, 1, 9, 5, 0, 0, 0},
+         {0, 9, 8, 0, 0, 0, 0, 6, 0},
+         /* |---------|---------|---------| */
+         {8, 0, 0, 0, 6, 0, 0, 0, 3},
+         {4, 0, 0, 8, 0, 3, 0, 0, 1},
+         {7, 0, 0, 0, 2, 0, 0, 0, 6},
+         /* |---------|---------|---------| */
+         {0, 6, 0, 0, 0, 0, 2, 8, 0},
+         {0, 0, 0, 4, 1, 9, 0, 0, 5},
+         {0, 0, 0, 0, 8, 0, 0, 7, 9}};
     MySudoku testSudoku = new MySudoku();
-    testSudoku.checkSudoku();
+
+
+
+    testSudoku.checkSudoku(testSpielFeld);
+
   }
 
-  public void checkSudoku() {
+  public void checkSudoku(int[][] spielFeld) {
 
     int value;
     boolean success = false;
