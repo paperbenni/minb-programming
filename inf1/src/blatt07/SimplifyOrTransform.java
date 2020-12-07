058a2f618b58a89591be1d4ccfb29dad6c57c0c2
paperbenni <paperbenni@gmail.com>
2020-12-07 16:59:41 +0100

    blatt 07 fertig

---
+++ inf1/src/blatt07/SimplifyOrTransform.java
@@ -0,0 +1,154 @@
+package blatt07;
+
+public class SimplifyOrTransform {
+	
+	/**  Formen Sie bitte die folgenden Ausdrücke etc. in vereinfachte Varianten um: */  
+	static void toBeSimplified()
+	{
+		int a, b, c;
+		
+		// Beispiel: Mehrfache Initialisierung mit dem selben Wert:
+		a = 0; b = 0; c = 0;
+		// Vereinfacht:
+		a = b = c = 0; 
+
+		a = (b = 1 + (c = 0));
+		// Vereinfacht:
+		a = b = 1; c = 0;
+	    	
+		a = b++ - (b += 2) - (b = 0);
+		// Vereinfacht:
+        b = 0; a = -3;
+		
+		// Ändern Sie bitte ab auf, wo passend, Dekrement/Inkement-Opratoren 
+		// oder kombinierte Zuweisungsoperatoren (jeweils ohne die Ausgabe zu ändern ...)
+		final int N0 = 0, N1 = 17;
+		for(int i = N0; i < N1; i++) {
+			System.out.print(i + ", ");
+		}
+
+		System.out.println();
+		for(int i = N1 - 1; i >= N0; i--) {
+			System.out.print(i + ", ");
+		}
+
+		System.out.println();
+		for(int i = N0; i < N1; i += 2) {
+			System.out.print(i + ", ");
+		}
+
+		System.out.println();
+		for(int i = N1 - 1; i >= N0; i -= 2) {
+			System.out.print(i + ", ");
+		}
+
+		System.out.println();
+		for(int i = N0 + 1; i < N1 * N1; i += i) {
+			System.out.print(i + ", ");
+		}
+		System.out.println();
+		
+		// Formen Sie bitte die folgende while-Schleifen in for-Schleifen um 
+		// und verwenden Sie wieder Inkrementoperatoren/kombinierte Zuweisungsoperatoren 
+
+		final int NMAX = 10;
+
+		a = 1;
+		while(a < NMAX)
+		{
+			b = 1;
+			while(b < NMAX) {
+				System.out.printf("%3d", a * b);
+				b = b + 1;
+			}
+			System.out.println();
+			a = a + 1;
+		}
+
+        for(a = 1; a < NMAX; a++) {
+            for (b = 1; b < NMAX; b++) {
+				System.out.printf("%3d", a * b);
+            }
+        }
+
+		System.out.println();
+		a = 3; 
+		while(a < 100)
+		{
+			b = 2;
+			while(b*b < a && a % b != 0) {
+				b = b + 1;
+			}
+			if(b * b > a) {
+				System.out.print(a + ", ");
+			}
+			a = a + 2;
+		}
+
+        for(a = 3; a < 100; a += 2) {
+            for (b = 2; b*b < a && a % b != 0; b++);
+            if (b * b > a) {
+                System.out.print(a + ", ");
+            }
+        }
+		
+		// Transformieren Sie bitte folgende If-/Else-Kette in eine Switch-Anweisung
+		int zufallsZahl = 1  + (int) (Math.random() * 6.);
+
+		String seite;
+		if(zufallsZahl == 1) {
+			seite = "Eins"; 
+		}
+		else if(zufallsZahl == 2) {
+			seite = "Zwei"; 
+		}
+		else if(zufallsZahl == 3) {
+			seite = "Drei"; 
+		}
+		else if(zufallsZahl == 4) {
+			seite = "Vier"; 
+		}
+		else if(zufallsZahl == 5) {
+			seite = "Fünf"; 
+		}
+		else if(zufallsZahl == 6) {
+			seite = "Sechs"; 
+		}
+		else  {
+			seite = "Ungültig"; 
+		}
+		System.out.println("\nGewürfelt: " + seite );
+
+
+        switch(zufallsZahl) {
+            case 1:
+                seite = "Eins"; 
+                break;
+            case 2:
+                seite = "Zwei"; 
+                break;
+            case 3:
+                seite = "Drei"; 
+                break;
+            case 4:
+                seite = "Vier"; 
+                break;
+            case 5:
+                seite = "Fünf"; 
+                break;
+            case 6:
+                seite = "Sechs"; 
+                break;
+            default:
+                seite = "Ungültig"; 
+                break;
+        }
+
+
+	}
+
+	public static void main(String[] args) {
+		toBeSimplified();
+	} 
+
+}
---
+++ inf1/src/blatt07/TernarySalutation.java
@@ -0,0 +1,40 @@
+package blatt07;
+
+public class TernarySalutation {
+  /**
+   * Alle möglichen Grußformeln ausgeben
+   */
+  private static void testSalutation() {
+    boolean bools[] = {true, false};
+    for (boolean firstNameBase : bools) {
+      for (boolean signature : bools) {
+        System.out.println(
+            "\n..........<Mail-Text>..........\n"); // Platzhalter
+        System.out.println(salutation(firstNameBase, signature, "Kim", "May",
+                                      "Raum E502\nTel.: 01234/567890"));
+        System.out.println("\n"); // Abstand zur uebersichtlichen Ausgabe
+      }
+    }
+  }
+
+  /**
+   * Grußformeln abhängig vom Bekanntheitsgrad und wahlweise mit oder ohne
+   * Signatur ausgeben
+   * @return Grußformeln
+   * @param onFirstNameBase Per-Du?
+   * @param signature Signatur anfügen?
+   * @param given	Vorname
+   * @param name Nachname
+   * @param signatureText Signatur
+   */
+
+  static String salutation(boolean onFirstNameBase, boolean signature,
+                           String given, String name, String signatureText) {
+    return (onFirstNameBase
+                ? "Viele Gruse\n" + given
+                : "Mit freundlichen Grusen\n" + given + " " + name) +
+        (signature ? "\n____________________\n" + signatureText : "");
+  }
+
+  public static void main(String[] args) { testSalutation(); }
+}
