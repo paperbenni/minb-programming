c15829d0b8519d7bb1c11b7d3f0062ca4027a17f
paperbenni <paperbenni@gmail.com>
2020-12-18 15:37:59 +0100

    finish blatt08

--- inf1/src/blatt08/RecCounter.java
+++ inf1/src/blatt08/RecCounter.java
@@ -9,8 +9,30 @@ public class RecCounter {
         }
     }
 
+    // Tiefe = sqrt(n*n)
+    public static void prtSqr1234(long n) {
+        if (n != 0) {
+            prtSqr1234(n - 1);
+            System.out.printf("%d, ", n*n);
+        }
+    }
+    public static void prt2468(long n) {
+        if ((n % 2) == 1)
+            n -= 1;
+        if (n != 0) {
+            prt2468(n - 2);
+            System.out.printf("%d, ", n);
+        }
+    }
+
 	public static void main(String[] args) {
-        prt1234(Long.parseLong(args[0]));
+        long n = Long.parseLong(args[0]);
+        System.out.println("1234:");
+        prt1234(n);
+        System.out.println("\nsquare:");
+        prtSqr1234(n);
+        System.out.println("\n2468:");
+        prt2468(n);
 	}
 
 }
