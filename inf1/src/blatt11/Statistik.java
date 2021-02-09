0f501f1a61f1699bb5f8864849a3fcab779fe7c7
paperbenni <paperbenni@gmail.com>
2021-02-09 12:44:33 +0100

    reformatting

--- inf1/src/blatt11/Statistik.java
+++ inf1/src/blatt11/Statistik.java
@@ -11,98 +11,100 @@ import java.nio.file.Paths;
  *
  */
 public class Statistik {
-	private double summe;
+    private double summe;
 
-	protected double n;
+    protected double n;
 
-	String name;
+    String name;
 
-	/**
-	 * Konstruktor
-	 * 
-	 * @param name Identifikator/Name für die Statistik
-	 */
-	public Statistik(String name) {
-		if (name == null) {
-			NullPointerException e = new NullPointerException();
-			throw e;
-		} else if (name.length() == 0 || ! name.matches("^[a-zA-Z]")) {
-			RuntimeException e = new RuntimeException();
-			throw e;
-		}
-		this.name = name;
-		this.summe = this.n = 0.;
-	}
+    /**
+     * Konstruktor
+     * 
+     * @param name Identifikator/Name für die Statistik
+     */
+    public Statistik(String name) {
+        if (name == null) {
+            NullPointerException e = new NullPointerException("Statistic name may be uninitialized");
+            throw e;
+        } else if (name.length() == 0) {
+            RuntimeException e = new RuntimeException("Empty name not allowed");
+            throw e;
+        } else if (!name.matches("^[a-zA-Z].*")) {
+            RuntimeException e = new RuntimeException("Name must start with a letter");
+            throw e;
+        }
+        this.name = name;
+        this.summe = this.n = 0.;
+    }
 
-	/**
-	 * Einen neuen Wert der Statistik hinzufügen
-	 * 
-	 * @param wert
-	 */
-	public void addWert(double wert) {
-		this.n++;
-		this.summe += wert;
-	}
+    /**
+     * Einen neuen Wert der Statistik hinzufügen
+     * 
+     * @param wert
+     */
+    public void addWert(double wert) {
+        this.n++;
+        this.summe += wert;
+    }
 
-	/**
-	 * * Zwei Werte dividieren * @param z, Zahl die dividiert wird (Zähler) * @param
-	 * n, Zahl durch die dividiert wird (Nenner) * @return z / n
-	 */
-	public static int div(int z, int n) {
-		try {
-			return z / n;
-		} catch (ArithmeticException e) {
-			System.err.println("Error: division by 0");
-			throw e;
-		}
-	}
+    /**
+     * * Zwei Werte dividieren * @param z, Zahl die dividiert wird (Zähler) * @param
+     * n, Zahl durch die dividiert wird (Nenner) * @return z / n
+     */
+    public static double div(int z, int n) {
+        try {
+            return z / n;
+        } catch (ArithmeticException e) {
+            System.err.println("Error: division by 0");
+            e.printStackTrace();
+            return Double.POSITIVE_INFINITY;
+        }
+    }
 
-	/**
-	 * Durchschnitt berechnen
-	 * 
-	 * @return Durchschnitt aller Werte
-	 */
-	double getDurchschnitt() {
-		if (this.n == 0) {
-			RuntimeException e = new RuntimeException();
-			throw e;
-		}
-		return this.summe / this.n;
-	}
+    /**
+     * Durchschnitt berechnen
+     * 
+     * @return Durchschnitt aller Werte
+     */
+    double getDurchschnitt() {
+        if (this.n == 0) {
+            RuntimeException e = new RuntimeException("empty statistic cannot be serialized");
+            throw e;
+        }
+        return this.summe / this.n;
+    }
 
-	/**
-	 * Statistik in Zeichenkette ausgeben
-	 * 
-	 */
-	@Override
-	public String toString() {
-		if (this.n == 0) {
-			RuntimeException e = new RuntimeException();
-			throw e;
-		}
-		return String.format("%12s: n==%10.0f, µ==%8f", this.name, this.n, this.getDurchschnitt());
-	}
+    /**
+     * Statistik in Zeichenkette ausgeben
+     * 
+     */
+    @Override
+    public String toString() {
+        if (this.n == 0) {
+            RuntimeException e = new RuntimeException("empty statistic cannot be serialized");
+            throw e;
+        }
+        return String.format("%12s: n==%10.0f, µ==%8f", this.name, this.n, this.getDurchschnitt());
+    }
 
-	public static void main(String[] args) {
-		Statistik stat = new Statistik("helloworld");
-		System.out.println("hello" + stat.div(1, 0));
-	}
+    public static void main(String[] args) {
+        System.out.println("test: " + div(1, 0));
+    }
 
-	/**
-	 * Statistik in Datei mit dem Namen der Statistik und der Endung ".statistik"
-	 * ausgeben
-	 * 
-	 * @throws IOException
-	 * 
-	 * @see https://stackoverflow.com/questions/2885173/how-do-i-create-a-file-and-write-to-it-in-java
-	 */
-	public void writeToFile() throws IOException {
-		try {
-			Files.write(Paths.get(this.name + ".statistik"), this.toString().getBytes());
-		} catch (IOException e) {
-			System.out.println(e);
-			throw e;
-		}
-	}
+    /**
+     * Statistik in Datei mit dem Namen der Statistik und der Endung ".statistik"
+     * ausgeben
+     * 
+     * @throws IOException
+     * 
+     * @see https://stackoverflow.com/questions/2885173/how-do-i-create-a-file-and-write-to-it-in-java
+     */
+    public void writeToFile() throws IOException {
+        try {
+            Files.write(Paths.get(this.name + ".statistik"), this.toString().getBytes());
+        } catch (IOException e) {
+            throw e;
+        }
+    }
 
 }
