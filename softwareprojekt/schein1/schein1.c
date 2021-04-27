e48f077b1483138d4c014f3971e16956e37e31d6
paperbenni <paperbenni@gmail.com>
2021-04-27 17:32:16 +0200

    finish schein1

--- softwareprojekt/schein1/schein1.c
+++ softwareprojekt/schein1/schein1.c
@@ -6,23 +6,77 @@
 char *targetstring;
 char userinput[100];
 
-int main(int argc, char *argv[])
-{
-    int i;
-    int position;
-    if (argc >= 2) {
-        targetstring = argv[1];
-    } else {
-        printf("enter string (please)> ");
-        fgets(userinput, sizeof(userinput), stdin);
-        targetstring = userinput;
-    }
+typedef enum { OK, FAIL } Test;
 
-    for (i = 0; targetstring[i] != '\0'; ++i) {
-        if (*(targetstring + i) == ':' && *(targetstring + i + 1) == ':') {
-            position = i + 2;
-        }
+// extract nach beispielen
+char *extract(char *input) {
+  int i;
+  int position;
+
+  for (i = 0; input[i] != '\0'; ++i) {
+    if (*(input + i) == ':' && *(input + i + 1) == ':') {
+      position = i + 2;
     }
-    printf("%s\n", targetstring + position);
-    return 0;
+  }
+  return input + position;
+}
+
+int stringcompare(char *a, char *b) {
+  int i;
+  int ret = 0;
+  i = 0;
+  while (*a || *b) {
+    if (*a != *b) {
+      ret = 1;
+      break;
+    }
+    a++;
+    b++;
+  }
+  return ret;
+}
+
+Test testColonparser(char *input, char *expected) {
+  Test t;
+  if (stringcompare(extract(input), expected) == 0)
+    t = OK;
+  else
+    t = FAIL;
+  return t;
+}
+
+typedef struct {
+  char *input;
+  char *expected;
+} TestCase;
+
+void runTests(int no, TestCase test[]) {
+  Test t;
+  int i;
+
+  for (i = 0; i < no; i++) {
+    printf("Test %d: ", i);
+    t = testColonparser(test[i].input, test[i].expected);
+    if (OK == t)
+      printf("OK \n");
+    if (FAIL == t)
+      printf("FAIL \n");
+  }
+}
+
+int main(int argc, char *argv[]) {
+  const int testNo = 8;
+  TestCase tests[8] = {
+      {"aaaa::bbb", "bbb"},
+      {"aaaa::bbb::g", "g"},
+      {"hello::world:", "world:"},
+      {"::hello_world", "hello_world"},
+      {"hello_world::", ""},
+      {"aaaaaaa::aaaaaaaaaaa::aaaaaa", "aaaaaa"},
+      {"this:is:a::test", "test"},
+      {"I::don't think there are a lot more ::special cases to cover",
+       "special cases to cover"},
+  };
+
+  runTests(testNo, tests);
 }
