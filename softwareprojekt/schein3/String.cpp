77439a79401f7a11efbc68e93bd2403177cac09b
paperbenni <paperbenni@gmail.com>
2021-05-24 19:55:13 +0200

    probably finished

--- softwareprojekt/schein3/String.cpp
+++ softwareprojekt/schein3/String.cpp
@@ -4,72 +4,107 @@ using namespace std;
 
 #include "String.h"
 
-
-String::String() { 
-    size = 0;
-    str = new char[1];
-    str[0] = '\0';
+String::String() {
+  size = 0;
+  str = new char[1];
+  str[0] = '\0';
 }
 
+void copy(char *s, int n, char *t) {
+  int i = 0;
+  while (i < n) {
+    t[i] = s[i];
+    i++;
+  }
+}
 
 String::String(char c) {
-    size = 1;
-    str = new char[2];
-    str[0] = c;
-    str[1] = '\0';
+  size = 1;
+  str = new char[2];
+  str[0] = c;
+  str[1] = '\0';
 }
 
 // copy
 // literal init
 
 String::String(const char *s) {
-    int i = 0;
-    int length = 0;
-    size = 0;
-    while (s[i] != '\0') {
-        i++;
-    }
+  int i = 0;
+  int length = 0;
+  size = 0;
+  while (s[i] != '\0') {
+    i++;
+  }
 
-    size = i;
-    i = 0;
+  size = i;
+  i = 0;
 
-    str = new char[size];
+  str = new char[size];
 
-    while (s[i] != '\0') {
-        str[i] = s[i];
-        i++;
-    }
+  while (s[i] != '\0') {
+    str[i] = s[i];
+    i++;
+  }
+}
+
+// copy
+// done
+String::String(const String &s) {
+  this->size = s.size;
+  this->str = new char[s.size];
+  copy(s.str, s.size, this->str);
 }
 
 // move
-String::String(const String& s) {
-  // TODO
-}
-
-// ??
-String::String(String&& s) {
-  // TODO
+String::String(String &&s) {
+  this->size = s.size;
+  /* delete[] this->str; */
+  this->str = s.str;
+  s.str = nullptr;
 }
 
 // destructor
 String::~String() { delete[] str; }
 
-char& String::operator[](int index) {
-  // TODO
-}
+char &String::operator[](int index) { return this->str[index]; }
 
 // copy
-String& String::operator=(const String& s) {
-  // TODO
+String &String::operator=(const String &s) {
+  this->size = s.size;
+  this->str = new char[s.size];
+  copy(s.str, s.size, this->str);
+  return *this;
 }
 
 // move
-String& String::operator=(String&& s) {
-  // TODO
+String &String::operator=(String &&s) {
+    if (this->size > 0)
+      delete[] this->str;
+  this->size = s.size;
+  this->str = s.str;
+  s.str = nullptr;
+  return *this;
 }
 
 // append
-String& String::operator+=(String& s) {
-  // TODO
-}
+// done
+String &String::operator+=(String &s) {
+  int newsize = s.size + this->size;
+  char *tmp = new char[newsize];
+  int i;
 
+  i = 0;
+  while (i < newsize) {
+    if (i < this->size) {
+      tmp[i] = this->str[i];
+    } else {
+      tmp[i] = s.str[i - this->size];
+    }
+    i++;
+  }
+
+  this->size = newsize;
+  delete[] this->str;
+  this->str = tmp;
+  return *this;
+}
--- softwareprojekt/schein3/main.cpp
+++ softwareprojekt/schein3/main.cpp
@@ -1,20 +1,19 @@
 #include "String.h"
 
-ostream& operator<< (ostream &out, String &s) {
-  for(int i=0; i<s.size; i++) {
+ostream &operator<<(ostream &out, String &s) {
+  for (int i = 0; i < s.size; i++) {
     out << s.str[i];
   }
   return out;
 }
 
-
 int main() {
-  String s;
-  String s2('H');
-  String s3("Hello World!");
 
-  cout << s << endl;
+  String s1;
+  String s2("Hello");
+  String s3(s2);
+  s1 += s2;
+  s2 = s3;
   cout << s2 << endl;
-  cout << s3 << endl;
-
+  cout << s2[2] << endl;
 }
